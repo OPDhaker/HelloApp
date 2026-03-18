15f4885e0518613e1cfce734245dc6b56936f52d
Om Pratap Dhaker <OPDhaker2007@gmail.com>
2026-03-18 08:59:05 +0530

    implement uc5

--- HelloApp.java
+++ HelloApp.java
@@ -2,14 +2,17 @@ public class HelloApp {
     public static void main(String[] args) {
         String greetingTarget;
 
-        if (args.length > 0) {
-            String[] names = new String[args.length];
-            for (int i = 0; i < args.length; i++) {
-                names[i] = args[i];
-            }
-            greetingTarget = String.join(", ", names);
-        } else {
+        if (args.length == 0) {
             greetingTarget = "World";
+        } else {
+            StringBuilder names = new StringBuilder();
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+            greetingTarget = names.toString();
         }
 
         System.out.println("Hello, " + greetingTarget + "!");
